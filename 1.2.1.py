f2793f6398b757b5f6442c365ddd6f11481459e0
MagWhitmer7 <55501077+MagWhitmer7@users.noreply.github.com>
2019-10-24 11:34:07 -0400

    Almost done

---
+++ 1.2.1.py
@@ -0,0 +1,80 @@
+# a121_catch_a_turtle.py
+#-----import statements-----
+import turtle as trtl
+import random
+
+#-----game configuration----
+shape = "arrow"
+size= 3
+colour= "palegreen"
+score= 0
+
+font_setup = ("Times New Roman", 20, "italic")
+timer = 5
+counter_interval = 1000   #1000 represents 1 second
+timer_up = False
+
+#-----initialize turtle-----
+neb = trtl.Turtle(shape=shape)
+neb.turtlesize(size)
+neb.fillcolor(colour)
+neb.pencolor(colour)
+neb.speed(0)
+
+keeper = trtl.Turtle(shape="circle")
+keeper.ht()
+keeper.penup()
+keeper.goto(-350, 250)
+keeper.pendown()
+keeper.pencolor("limegreen")
+font = ("Times New Roman", 30, "bold")
+
+counter =  trtl.Turtle()
+counter.ht()
+counter.penup()
+counter.goto(290, -350)
+counter.pendown()
+counter.pencolor("forestgreen")
+
+#-----game functions--------
+def turtle_clicked (x,y):
+    move_neb()
+    score_keeper()
+def move_neb ():
+    neb.penup()
+    neb.ht()
+    new_xpos= random.randint(-400, 400)
+    new_ypos= random.randint(-300, 300)
+    neb.goto(new_xpos, new_ypos)
+    neb.showturtle()     
+def score_keeper ():
+    global score
+    score+=1
+    keeper.clear()
+    keeper.write (score, font=font)
+
+def countdown():
+  global timer, timer_up
+  counter.clear()
+  if timer <= 0:
+    counter.write("Time's Up", font=font_setup)
+    timer_up = True
+    game_over()
+  else:
+    counter.write("Timer: " + str(timer), font=font_setup)
+    timer -= 1
+    counter.getscreen().ontimer(countdown, counter_interval) 
+def game_over ():
+    neb.penup()
+    neb.goto(500 , 500)
+    wn.bgcolor("crimson")
+
+
+
+#-----events----------------
+neb.onclick(turtle_clicked)
+
+
+wn = trtl.Screen()
+wn.ontimer(countdown, counter_interval)
+wn.mainloop()
